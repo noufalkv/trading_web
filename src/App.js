454087e6f4a6c226e283568f5f7400087dcb258d
Noufal <noufal.vadakkyil@raknbank.ae>
2025-10-25 01:12:44 +0400

    fix: update socket connection to use environment variable for server URL

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
       timeVisible: true,
     });
 
-    const socket = io("http://localhost:4000", {
+    const socket = io(process.env.REACT_APP_SERVER_BASE_URL || "http://localhost:4000", {
       withCredentials: true,
       extraHeaders: {
         access_token:token
